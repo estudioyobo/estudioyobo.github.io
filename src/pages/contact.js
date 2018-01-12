2f5c0ccafa93e8010adae2aae80a11f4be49f9c0
Jesús Gallego Irles <jesusgallego@outlook.com>
2018-01-13 00:22:32 +0100

    Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 
 import mapConfig from "../config/map";
 
+import "./contact.css";
+
 class Contact extends Component {
   componentDidMount() {
     var script = document.createElement("script");
-    script.innerText = `function myMap() { var mapOptions = { center: new google.maps.LatLng(38.265277, -0.704316), zoom: 10, mapTypeId: google.maps.MapTypeId.HYBRID };var map = new google.maps.Map(document.getElementById("map"), mapOptions);}`;
+    script.innerText =
+      "function myMap() { var myLatLng = {lat: 38.265277, lng: -0.704316};var mapOptions = { center: myLatLng, zoom: 17, mapTypeId: google.maps.MapTypeId.HYBRID };var map = new google.maps.Map(document.getElementById('map'), mapOptions);var marker = new google.maps.Marker({position: myLatLng,map: map,title: 'YOBO'});}";
     document.getElementById("map").appendChild(script);
 
     var g = document.createElement("script");
@@ -21,15 +24,24 @@ class Contact extends Component {
     return (
       <div>
         <h1>CONTACTO</h1>
-        <form>
-          <label htmlFor="name">Nombre:</label>
-          <input name="name" type="text" />
-          <label htmlFor="email">Email:</label>
-          <input name="email" type="email" />
-          <textarea name="message" id="message" cols="30" rows="10" />
-          <input type="submit" value="Enviar" />
-        </form>
-        <div id="map" />
+        <div className="container">
+          <form className="form">
+            <div className="form-group">
+              <label htmlFor="name">Nombre:</label>
+              <input name="name" type="text" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input name="email" type="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Mensaje:</label>
+              <textarea name="message" id="message" cols="30" rows="10" />
+            </div>
+            <input type="submit" value="Enviar" />
+          </form>
+          <div id="map" className="map" />
+        </div>
       </div>
     );
   }
