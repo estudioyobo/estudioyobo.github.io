500598e0b8035da82f6d793289753cc4b6aaa431
Jesús Gallego Irles <jesusgallego@outlook.com>
2018-01-15 11:58:04 +0100

    Service and Contact Style

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import "./service.css";
+
 const Service = ({ title, description, list, image }) => (
   <div className="service">
     <div className="service--content">
       <h2 className="service--title">{title}</h2>
       <p className="service--description">{description}</p>
-      <ul className="service--list">{list.map(item => <li>{item}</li>)}</ul>
+      <ul className="service--list">
+        {list.map((item, i) => <li>{item}</li>)}
+      </ul>
     </div>
     {image && <img className="service--image" src={image} alt="" />}
   </div>
--- src/pages/services.js
+++ src/pages/services.js
@@ -6,7 +6,7 @@ import services from "../config/services";
 const Services = () => (
   <div>
     <h1>SERVICIOS</h1>
-    {services.map(service => <Service {...service} />)}
+    {services.map((service, i) => <Service {...service} key={i} />)}
   </div>
 );
 
