535af95b756ead6ca15e1cc9dc4882963c6814d5
Jesús Gallego Irles <jesusgallego@outlook.com>
2018-01-17 12:20:34 +0100

    Google Fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: "Yobo"
   },
-  plugins: ["gatsby-plugin-react-helmet"]
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: ["Montserrat:200,500,700"]
+      }
+    }
+  ]
 };
