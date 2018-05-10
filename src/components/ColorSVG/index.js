9518f216839e8607dbae8daf8554af1ab1fbccc7
Jesús Gallego Irles <jesusgallego@outlook.com>
2018-05-10 15:35:26 +0200

    Mobile friendly

--- src/components/ColorSVG/index.js
+++ src/components/ColorSVG/index.js
@@ -3,14 +3,10 @@ import React from "react";
 import styled from "styled-components";
 
 const SVG = styled.object`
-  position: absolute;
   display: ${props => (props.hover ? "none" : "block")};
 `;
 const Wrapper = styled.div`
   position: relative;
-  &:hover object:first-child {
-    display: block;
-  }
 `;
 
 const ColorSVG = ({ wrapperClass, image, color }) => {
