5ff0eb24803d410a7bd5fe66de2dc2d5761a7853
Jesús Gallego Irles <jesusgallego@outlook.com>
2018-07-30 13:26:29 +0200

    Landing with grid filling width

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,16 +19,7 @@ const TemplateWrapper = ({ children, data }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: "70px auto 100px",
-        maxWidth: 960,
-        padding: "0px 1.0875rem 1.45rem",
-        paddingTop: 0
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
     <Footer {...info} />
   </div>
 );
