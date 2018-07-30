6735e7edf6ce486033f988115d8a7a26d0d1b080
Jesús Gallego Irles <jesusgallego@outlook.com>
2018-07-30 13:26:36 +0200

    Up button in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./footer.css";
 
+function scrollTop() {
+  scrollTo({ top: 0, left: 0, behavior: "smooth" });
+}
+
 const Social = ({ name, icon, link }) => (
   <div className="social">
     <a href={link} target="_blank">
@@ -21,6 +25,10 @@ const Footer = ({ social, mail, phone }) => {
       </div>
       <div className="footer-phone">
         {phone.map((p, i) => <span key={i}>{p}</span>)}
+      </div>
+      <div className="footer__up" onClick={scrollTop}>
+        <span className="footer__up--arrow">▲</span>
+        <span className="footer__up--text">UP</span>
       </div>
     </footer>
   );
