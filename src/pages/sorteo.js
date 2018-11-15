e0e7219d19e95f3d2364fabea899aececf04137d
Alejandro Almira <aalmiramolla@gmail.com>
2018-11-15 20:29:19 +0100

    Added draw page without dates

---
+++ src/pages/sorteo.js
@@ -0,0 +1,230 @@
+import React from "react";
+import styled from "styled-components";
+import Layout from "../components/Layout";
+
+const Wrapper = styled.div`
+max-width: 960px;
+margin: 0 auto 100px;
+`;
+
+const Sorteo = () => (
+  <Layout>
+    <Wrapper>
+      <h1>Bases del sorteo</h1>
+      <h2>Compañía organizadora</h2>
+      <p>
+        Estudio Yobo, organiza un SORTEO en el que sortea el diseño de un
+        logotipo y una landing web o landing app.
+      </p>
+      <h2>Desvinculación con respecto a Facebook</h2>
+      <p>
+        Facebook no patrocina, avala ni administra de modo alguno esta
+        promoción, ni está asociado a ella. El usuario se desvincula totalmente
+        de Facebook y es consciente de que la información la proporciona la
+        empresa Estudio Yobo y no Facebook. La información proporcionada se
+        utilizará para gestionar la participación del concursante y para
+        comunicarle el premio, en caso de salir ganador.
+      </p>
+      <h2>Objeto, mecánica del concurso y requisitos</h2>
+      <p>
+        A través de este concurso se procederá al sorteo de el diseño de un
+        logotipo y una landing web o landing app.
+      </p>
+      <ul>
+        <li>
+          Seguir en Instagram a{" "}
+          <a target="_blank" href="https://www.instagram.com/estudioyobo/">
+            @estudioyobo
+          </a>.
+        </li>
+        <li>
+          Compartir la historia destacada del sorteo de nuestro Instagram en tus
+          historias de Instagram mencionando a @estudioyobo en ella.
+        </li>
+        <li>
+          Darle Like a nuestra{" "}
+          <a target="_blank" href="https://www.facebook.com/EstudioYobo/">
+            página de Facebook
+          </a>.
+        </li>
+        <li>
+          Compartir la publicación del sorteo en tu muro de Facebook de manera
+          pública.
+        </li>
+        <li>Hacer todo lo anterior antes del día xx/xx+1/2018.</li>
+      </ul>
+      <p>
+        Para poder participar en este sorteo, el usuario tiene que ser FAN de la
+        página de Facebook Estudio Yobo, indicar ME GUSTA en la publicación
+        correspondiente al sorteo. Como en cualquier otra página de Facebook,
+        para ser Fan, solo hay que pinchar en la parte superior de la página
+        donde indica Me gusta y a partir de ese momento se podrá seguir todo el
+        contenido que se publique. Una vez que se es Fan, recibirá las últimas
+        publicaciones y sorteos que se realicen.
+      </p>
+      <p>
+        La participación en el Concurso supone la cesión y autorización expresa
+        de todos los derechos de propiedad intelectual y de imagen sobre las
+        participaciones, de forma que el participante cede Estudio Yobo los
+        derechos de explotación de naturaleza intelectual, industrial y/o de
+        imagen que pudieran corresponderle o derivar de la participación en el
+        concurso.
+      </p>
+      <h2>Duración y Ámbito</h2>
+      <p>La actividad se desarrollará en todo el territorio nacional.</p>{" "}
+      <p>
+        La fecha será del xx/xx/2018 hasta el xx/xx/2018 incluido ultimo día
+        también para participar.
+      </p>
+      <h2>Requisitos para participar</h2>
+      <p>
+        Podrán participar en la promoción todas las personas físicas, mayores de
+        18 años, con residencia en España, que sean fans de la página oficial de
+        Facebook Estudio Yobo, que cuenten con un perfil de usuario real en
+        Facebook e Instagram y cumplan con las mecánicas del concurso.
+      </p>
+      <h2>Selección del ganador, comunicación y entrega</h2>
+      <p>
+        El sorteo se realizará escogiendo de forma aleatoria a uno de los
+        participantes que haya seguido los requisitos necesarios y obligatorios
+        para entrar a participar en el sorteo.
+      </p>
+      <p>
+        Una vez realizado el sorteo y elegido el ganador se organizará una
+        reunión con él y se realizará un briefing* para enfocar la realización
+        del Logotipo y la Landing Web/App.
+      </p>
+      <p>
+        <b>Briefing*</b>: Hacen referencia a un documento o reunión informativa
+        que proporciona información a una entidad que lo crea o celebra.Es la
+        parte estratégica de la preparación de una acción publicitaria, de
+        diseño.
+      </p>
+      <p>
+        Los nombres de los ganadores se comunicarán en página de Facebook de
+        Estudio Yobo y en la sección de stories de Instagram de Estudio Yobo una
+        vez finalizada la promoción.
+      </p>
+      <p>
+        Nos pondremos en contacto con el afortunado a través de Facebook e
+        Instagram para concretar el día de la reunión.
+      </p>
+      <p>
+        Disponen de 7 DÍAS para contestarnos y reclamar su premio. Si no se
+        contesta realizaremos otro sorteo acabados esos 7 DÍAS y volveremos a
+        ponernos en contacto con el nuevo ganador.
+      </p>
+      <h2>Detalles del premio del sorteo:</h2>
+      <p>
+        En nuestro concurso se sortea el diseño y el desarrollo de un logotipo y
+        una landing page o landing app.
+      </p>
+      <ul>
+        <li>
+          Logotipo y Manual de Identidad Corporativa. Dos propuestas de logotipo
+          y una revisión. Además, se incluye versión en blanco y negro, a un
+          color y si hay más versiones cromáticas. El manual incluye los códigos
+          del color, la tipografía usada y las versiones cromáticas.
+        </li>
+        <li>
+          Landing Web o App. 1 Propuesta y 1 revisión. La Landing web/app se
+          compone de 4 secciones: Home, Contacto y dos secciones extra a elegir.
+        </li>
+      </ul>
+      <h2>Reservas y limitaciones</h2>
+      <p>
+        Una vez llegada la fecha de finalización del sorteo, no se podrá entrar
+        en él porque se estaría incumpliendo el punto de duración y ámbito.
+      </p>
+      <p>El logotipo se entregará en las versiones JPG, PNG, y SVG.</p>
+      <p>El manual de identidad se entrega en formato PDF.</p>
+      <p>
+        No se realizarán más revisiones que las que se mencionan anteriormente.
+      </p>
+      <p>
+        De las dos propuestas de logotipo realizadas, la elegida, será la que
+        tenga una revisión si el ganador lo solicita.
+      </p>
+      <p>
+        No se harán una revisión de cada propuesta de logotipo. Solo una de la
+        propuesta elegida.
+      </p>
+      <p>
+        El premio del logotipo se podrá tratar cómo rediseño del actual logotipo
+        del ganador.
+      </p>
+      <p>No retocamos Wordpress o similar.</p>
+      <p>No hacemos Wordpress o similar.</p>
+      <p>
+        En caso de escoger Landing Web, no nos hacemos responsables del
+        alojamiento de la web.
+      </p>
+      <p>
+        Estudio Yobo queda eximida de cualquier responsabilidad en el supuesto
+        de existir algún error en los datos facilitados por los propios
+        agraciados que impidiera su identificación. Igualmente no se
+        responsabiliza de las posibles pérdidas, deterioros, robos o cualquier
+        otra circunstancia imputable a correos que puedan afectar al envío de
+        los premios.
+      </p>
+      <p>
+        Estudio Yobo se reserva el derecho de emprender acciones judiciales
+        contra aquellas personas que realicen cualquier tipo de acto susceptible
+        de ser considerado manipulación o falsificación del concurso.
+      </p>
+      <p>
+        Estudio Yobo excluye cualquier responsabilidad por daños y perjuicios de
+        toda naturaleza que puedan deberse a la falta temporal de disponibilidad
+        o de continuidad del funcionamiento de los servicios mediante los que se
+        participa en la promoción, a la defraudación de la utilidad que los
+        usuarios hubieren podido atribuir a los mismos, y en particular, aunque
+        no de modo exclusivo, a los fallos en el acceso a las distintas páginas
+        y envíos de respuestas de participación a través de Internet.
+      </p>
+      <p>
+        Estudio Yobo se reserva el derecho a efectuar cambios que redunden en el
+        buen fin del concurso cuando concurra causa justa o motivos de fuerza
+        mayor que impidan llevarla a término en la forma en que recogen las
+        presentes bases.
+      </p>
+      <p>
+        Estudio Yobo se reserva el derecho a aplazar o ampliar el período del
+        concurso, así como la facultad de interpretar las presentes bases
+        legales. Asimismo, Estudio Yobo quedará exenta de toda responsabilidad
+        si concurriere alguno de los casos señalados, así como de cualquier
+        responsabilidad por los daños y perjuicios que pudiesen ocasionarse
+        durante el disfrute del premio.
+      </p>
+      <h2>Protección de datos</h2>
+      <p>
+        Los datos suministrados por los participantes serán tratados
+        confidencialmente y recopilados en un fichero automatizado de datos de
+        carácter personal, siendo Estudio Yobo. titular y responsable de dicho
+        fichero, cuya finalidad será la gestión del presente sorteo. La empresa
+        garantiza el cumplimiento íntegro de la Ley Orgánica 15/1999, de 13 de
+        diciembre de Protección de Datos de Carácter Personal en el tratamiento
+        de los datos personales recogidos en el presente CONCURSO, en especial
+        por lo que se refiere a la atención del ejercicio de los derechos de
+        información, acceso, rectificación, oposición y cancelación de datos
+        personales de los concursantes.
+      </p>
+      <h2>Aceptación de las bases</h2>
+      <p>
+        La participación en el concurso implica la aceptación de las presentes
+        Bases, por lo que la manifestación en el sentido de no aceptación de la
+        totalidad o parte de las mismas implicará la exclusión del participante
+        y como consecuencia de ello, Estudio Yobo quedará liberada del
+        cumplimiento de la obligación contraída con dicho participante.
+      </p>
+      <p>¡¡Mucha suerte a todos!!</p>
+      <p>
+        <i>
+          Cualquier duda contacta con nosotros a través de{" "}
+          <a href="mailto:info@estudioyobo.com">info@estudioyobo.com</a>
+        </i>
+      </p>
+    </Wrapper>
+  </Layout>
+);
+
+export default Sorteo;
